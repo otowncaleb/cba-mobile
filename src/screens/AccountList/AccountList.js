3e01173e29b1463ecb97ac81257912fd2c77adf5
Caleb St-Denis <caleb.stdenis@gmail.com>
2018-10-28 15:45:31 -0400

    Initial AccountList page

--- src/screens/AccountList/AccountList.js
+++ src/screens/AccountList/AccountList.js
@@ -1,8 +1,57 @@
 import React from "react";
+import { StyleSheet } from "react-native";
 import { Screen } from "../../components";
+import { Card, Body, Right, Text, Icon, View, CardItem } from "native-base";
+
+const accounts = [
+  {
+    name: "Chequing",
+    balance: "$16,000"
+  },
+  {
+    name: "Savings",
+    balance: "$12,000"
+  },
+  {
+    name: "Credit",
+    balance: "$1,000"
+  }
+];
 
 export default class AccountList extends React.Component {
   render() {
-    return <Screen title="Accounts" navigation={this.props.navigation} />;
+    const listItems = accounts.map(account => (
+      <CardItem key={account.name} bordered button>
+        <Body>
+          <Text>{account.name}</Text>
+        </Body>
+        <Right>
+          <View style={styles.listItemRight}>
+            <Text style={styles.balance}>{account.balance}</Text>
+            <Icon name="arrow-forward" />
+          </View>
+        </Right>
+      </CardItem>
+    ));
+
+    return (
+      <Screen title="Accounts" {...this.props}>
+        <Card>{listItems}</Card>
+      </Screen>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  listItemRight: {
+    flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-end"
+  },
+  balance: {
+    marginRight: 20,
+    color: "#85bb65"
+  }
+});
