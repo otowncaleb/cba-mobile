65316a345749748cc9ec214af7a63a4e1ade7424
VPoint <estar.raji@gmail.com>
2018-11-01 20:35:49 -0400

    Added information to the Contact Us page.

--- src/screens/ContactUs/ContactUs.js
+++ src/screens/ContactUs/ContactUs.js
@@ -1,13 +1,91 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
-import { Screen } from "app/components";
+import {
+    Text,
+    Button,
+    Card,
+    Icon,
+    CardItem,
+    Body,
+    Content
+} from "native-base";
+import {Linking, StyleSheet} from "react-native";
+import {Screen} from "app/components";
 
 export default class ContactUs extends React.Component {
-  render() {
-    return (
-      <Screen>
-        <Text>Contact Us</Text>
-      </Screen>
-    );
-  }
+    onFindBranch = function () {
+        this.props.navigation.navigate("Locate a Branch");
+    };
+
+    callSupport = function (tel) {
+        Linking.openURL("tel:" + tel);
+    };
+
+    emailSupport = function (email) {
+        Linking.openURL("mailto:" + email);
+    };
+
+    render() {
+        return (
+            <Screen>
+                <Content>
+                    <Card>
+                        <CardItem bordered>
+                            <Body>
+                            <Text style={styles.headerPadding}>
+                                <Icon name="old-phone" type="Entypo"/> By phone :</Text>
+                            <Text>24 hours a day, 7 days a week.</Text>
+
+                            <Text>Service in English <Text style={styles.clickable}
+                                                           onPress={() => this.callSupport("1-800-555-5555")}>1-800-555-5555</Text> or <Text
+                                style={styles.clickable}
+                                onPress={() => this.callSupport("613-555-5555")}>613-555-5555</Text></Text>
+
+                            <Text>Service in French <Text style={styles.clickable}
+                                                          onPress={() => this.callSupport("1-800-444-4444")}>1-800-444-4444</Text></Text></Body>
+                        </CardItem>
+                        <CardItem bordered>
+                            <Body>
+                            <Text style={styles.headerPadding}><Icon name='mail'/> By email :</Text>
+                            <Text>We'll get back to you in under 24 hours!</Text>
+                            <Text>Service in both languages: <Text style={styles.clickable}
+                                                                   onPress={() => this.emailSupport("eraji035@uottawa.ca")}>abc@cba.ca</Text></Text>
+                            </Body>
+                        </CardItem>
+                        <CardItem bordered>
+                            <Body>
+                            <Text style={styles.headerPadding}>
+                                <Icon name="location" type="Entypo"/> Stop by anytime:</Text>
+                            <Text>Canadian Bank of Agriculture</Text>
+                            <Text>1234 Bank Avenue East</Text>
+                            <Text>Ottawa, Ontario</Text>
+                            <Text>P0P 0P0</Text>
+                            <Text/>
+                            <Text>And in case you need it, here’s our bank information:</Text>
+                            <Text>Institution #123</Text>
+                            <Text>Transit #00000</Text>
+                            </Body>
+                        </CardItem>
+                        <CardItem>
+                            <Body>
+                            <Button full title="locate" onPress={() => this.onFindBranch()}>
+                                <Text>Find Your Local Branch</Text>
+                            </Button>
+                            </Body>
+                        </CardItem>
+                    </Card>
+                </Content>
+            </Screen>
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    clickable: {
+        color: "#3F51B5",
+        fontWeight: "bold"
+    },
+    headerPadding: {
+        padding: 5,
+        fontWeight: "bold"
+    }
+});
