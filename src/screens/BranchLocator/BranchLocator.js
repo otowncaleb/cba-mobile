a8fc909759756414545fbcee010127c0d2db2d70
Caleb St-Denis <caleb.stdenis@gmail.com>
2018-11-20 15:52:09 -0500

    Find a Location Screen

--- src/screens/BranchLocator/BranchLocator.js
+++ src/screens/BranchLocator/BranchLocator.js
@@ -1,21 +1,154 @@
 import React from "react";
+import { StyleSheet } from "react-native";
 import { MapView } from "expo";
+import { View, Text, Badge, FooterTab, Button } from "native-base";
 import { Screen } from "app/components";
+import { locations } from "app/data";
+import { isCurrentDayOfWeek } from "./utils";
 
 export default class BranchLocator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      serviceFilter: props.navigation.getParam("filter", "ATM")
+    };
+  }
+
   render() {
+    const { serviceFilter } = this.state;
+
+    const markers = locations
+      .filter(location => location.services.includes(serviceFilter))
+      .map(this.renderMarker);
+
     return (
-      <Screen map>
+      <Screen map footer={this.renderFooter()}>
         <MapView
           style={{ flex: 1 }}
+          mapPadding={{
+            top: 100
+          }}
           initialRegion={{
             latitude: 45.4231,
             longitude: -75.6831,
             latitudeDelta: 0.06,
             longitudeDelta: 0.06
           }}
-        />
+        >
+          {markers}
+        </MapView>
       </Screen>
     );
   }
+
+  renderFooter() {
+    const { serviceFilter } = this.state;
+    return (
+      <FooterTab>
+        <Button
+          active={serviceFilter === "ATM"}
+          onPress={() => this.setState({ serviceFilter: "ATM" })}
+        >
+          <Text>ATMs</Text>
+        </Button>
+        <Button
+          active={serviceFilter === "Branch"}
+          onPress={() => this.setState({ serviceFilter: "Branch" })}
+        >
+          <Text>Branches</Text>
+        </Button>
+      </FooterTab>
+    );
+  }
+
+  renderMarker(location) {
+    const { id, coordinates, services, name, address, phone, hours } = location;
+
+    const header = (
+      <View style={styles.textContainer}>
+        <Text>{name}</Text>
+        {services.map(service => (
+          <Badge key={service} primary style={styles.locationTypeBadge}>
+            <Text style={styles.locationTypeText}>{service}</Text>
+          </Badge>
+        ))}
+      </View>
+    );
+
+    const addressInfo = (
+      <View style={styles.textContainer}>
+        <Text style={styles.calloutLabel}>Address: </Text>
+        <Text>{address}</Text>
+      </View>
+    );
+
+    const phoneInfo = services.includes("Branch") && (
+      <View style={styles.textContainer}>
+        <Text style={styles.calloutLabel}>Phone: </Text>
+        <Text>{phone}</Text>
+      </View>
+    );
+
+    const hoursInfo = services.includes("Branch") && (
+      <View style={styles.textContainer}>
+        <Text style={styles.calloutLabel}>Hours: </Text>
+        <View>
+          {Object.entries(hours).map(([day, hoursForDay]) => (
+            <View key={day} style={styles.textContainer}>
+              <Text
+                style={[
+                  styles.dayLabel,
+                  isCurrentDayOfWeek(day) && styles.currentDay
+                ]}
+              >
+                {day}
+              </Text>
+              <Text
+                style={
+                  (isCurrentDayOfWeek(day) && styles.currentDay) || undefined
+                }
+              >
+                {hoursForDay}
+              </Text>
+            </View>
+          ))}
+        </View>
+      </View>
+    );
+
+    return (
+      <MapView.Marker key={id} coordinate={coordinates}>
+        <MapView.Callout>
+          <View>
+            {header}
+            {addressInfo}
+            {phoneInfo}
+            {hoursInfo}
+          </View>
+        </MapView.Callout>
+      </MapView.Marker>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  textContainer: {
+    flex: 1,
+    flexDirection: "row"
+  },
+  locationTypeBadge: {
+    marginLeft: 4
+  },
+  locationTypeText: {
+    fontSize: 12
+  },
+  calloutLabel: {
+    fontWeight: "bold"
+  },
+  currentDay: {
+    fontWeight: "bold"
+  },
+  dayLabel: {
+    width: 110
+  }
+});
--- src/screens/ContactUs/ContactUs.js
+++ src/screens/ContactUs/ContactUs.js
@@ -1,84 +1,104 @@
 import React from "react";
-import {
-    Text,
-    Button,
-    Card,
-    Icon,
-    CardItem,
-    Body,
-    Content
-} from "native-base";
-import {Linking, StyleSheet} from "react-native";
-import {Screen} from "app/components";
+import { Text, Button, Card, Icon, CardItem, Body, Content } from "native-base";
+import { Linking, StyleSheet } from "react-native";
+import { Screen } from "app/components";
 
 export default class ContactUs extends React.Component {
-    onFindBranch = function () {
-        this.props.navigation.navigate("Locate a Branch");
-    };
+  onFindBranch = function() {
+    this.props.navigation.navigate("Find a Location", {
+      filter: "Branch"
+    });
+  };
 
-    callSupport = function (tel) {
-        Linking.openURL("tel:" + tel);
-    };
+  callSupport = function(tel) {
+    Linking.openURL("tel:" + tel);
+  };
 
-    emailSupport = function (email) {
-        Linking.openURL("mailto:" + email);
-    };
+  emailSupport = function(email) {
+    Linking.openURL("mailto:" + email);
+  };
 
-    render() {
-        return (
-            <Screen>
-                <Content>
-                    <Card>
-                        <CardItem bordered>
-                            <Body>
-                            <Text style={styles.headerPadding}>
-                                <Icon name="old-phone" type="Entypo"/> By phone</Text>
-                            <Text>24 hours a day, 7 days a week.</Text>
+  render() {
+    return (
+      <Screen>
+        <Content>
+          <Card>
+            <CardItem bordered>
+              <Body>
+                <Text style={styles.headerPadding}>
+                  <Icon name="old-phone" type="Entypo" /> By phone
+                </Text>
+                <Text>24 hours a day, 7 days a week.</Text>
 
-                            <Text>Service in English <Text style={styles.clickable}
-                                                           onPress={() => this.callSupport("1-800-555-5555")}>1-800-555-5555</Text></Text>
-                            <Text>Service in French <Text style={styles.clickable}
-                                                          onPress={() => this.callSupport("1-800-444-4444")}>1-800-444-4444</Text></Text></Body>
-                        </CardItem>
-                        <CardItem bordered>
-                            <Body>
-                            <Text style={styles.headerPadding}><Icon name='mail'/> By email</Text>
-                            <Text>{"We'll get back to you in under 24 hours!"}</Text>
-                            <Text>Service in both languages: <Text style={styles.clickable}
-                                                                   onPress={() => this.emailSupport("eraji035@uottawa.ca")}>abc@cba.ca</Text></Text>
-                            </Body>
-                        </CardItem>
-                        <CardItem bordered>
-                            <Body>
-                            <Text style={styles.headerPadding}>
-                                <Icon name="location" type="Entypo"/> Stop by anytime</Text>
-                            <Text>Canadian Bank of Agriculture</Text>
-                            <Text>1234 Bank Avenue East</Text>
-                            <Text>Ottawa, Ontario</Text>
-                            <Text>{"P0P 0P0"}</Text>
-                            </Body>
-                        </CardItem>
-                        <CardItem>
-                            <Body>
-                            <Button full title="locate" onPress={() => this.onFindBranch()}>
-                                <Text>Find Your Local Branch</Text>
-                            </Button>
-                            </Body>
-                        </CardItem>
-                    </Card>
-                </Content>
-            </Screen>
-        );
-    }
+                <Text>
+                  Service in English{" "}
+                  <Text
+                    style={styles.clickable}
+                    onPress={() => this.callSupport("1-800-555-5555")}
+                  >
+                    1-800-555-5555
+                  </Text>
+                </Text>
+                <Text>
+                  Service in French{" "}
+                  <Text
+                    style={styles.clickable}
+                    onPress={() => this.callSupport("1-800-444-4444")}
+                  >
+                    1-800-444-4444
+                  </Text>
+                </Text>
+              </Body>
+            </CardItem>
+            <CardItem bordered>
+              <Body>
+                <Text style={styles.headerPadding}>
+                  <Icon name="mail" /> By email
+                </Text>
+                <Text>{"We'll get back to you in under 24 hours!"}</Text>
+                <Text>
+                  Service in both languages:{" "}
+                  <Text
+                    style={styles.clickable}
+                    onPress={() => this.emailSupport("eraji035@uottawa.ca")}
+                  >
+                    abc@cba.ca
+                  </Text>
+                </Text>
+              </Body>
+            </CardItem>
+            <CardItem bordered>
+              <Body>
+                <Text style={styles.headerPadding}>
+                  <Icon name="location" type="Entypo" /> Stop by anytime
+                </Text>
+                <Text>Canadian Bank of Agriculture</Text>
+                <Text>1234 Bank Avenue East</Text>
+                <Text>Ottawa, Ontario</Text>
+                <Text>{"P0P 0P0"}</Text>
+              </Body>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Button full title="locate" onPress={() => this.onFindBranch()}>
+                  <Text>Find Your Local Branch</Text>
+                </Button>
+              </Body>
+            </CardItem>
+          </Card>
+        </Content>
+      </Screen>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    clickable: {
-        color: "#3F51B5",
-        fontWeight: "bold"
-    },
-    headerPadding: {
-        padding: 5,
-        fontWeight: "bold"
-    }
+  clickable: {
+    color: "#3F51B5",
+    fontWeight: "bold"
+  },
+  headerPadding: {
+    padding: 5,
+    fontWeight: "bold"
+  }
 });
